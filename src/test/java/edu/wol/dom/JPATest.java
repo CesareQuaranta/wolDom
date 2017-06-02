835f521787fcd1a634f4a35784eba02d4638f794
cesare.quaranta <cesare.quaranta@pronetics.it>
2017-06-03 00:37:12 +0200

    primo test jpa

---
+++ src/test/java/edu/wol/dom/JPATest.java
@@ -0,0 +1,25 @@
+package edu.wol.dom;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={ JpaTestConfig.class})
+public class JPATest {
+	@PersistenceContext(type=PersistenceContextType.EXTENDED)
+    EntityManager em;
+   
+    @Test
+	public void testUser() throws Exception{
+    	User newUser= new User("TestUser",new Prospective());
+    	em.persist(newUser);
+    	User user=em.find(User.class, "TestUser");
+    	Assert.assertNotNull("Fild User Failed", user);
+    }
+}
