16b2b63c2d683fe70d649e3000b2ca3c0d46cf95
cesare.quaranta <cesare.quaranta@pronetics.it>
2017-06-28 18:42:11 +0200

    Prima versione generazione vertici

--- src/main/java/edu/wol/dom/shape/AsteroidShapeFactory.java
+++ src/main/java/edu/wol/dom/shape/AsteroidShapeFactory.java
@@ -1,10 +1,38 @@
 package edu.wol.dom.shape;
 
-public class AsteroidShapeFactory {
+import java.util.ArrayList;
+import java.util.List;
 
-	public static AsteroidShape generateShape(){
+import edu.wol.dom.space.Vector;
+
+public class AsteroidShapeFactory {
+	private static AsteroidShapeFactory instance=null;
+	public static AsteroidShapeFactory getInstance(){
+		if(instance==null){
+			instance = new AsteroidShapeFactory();
+		}
+		return instance;
+	}
+	public AsteroidShape generateShape(){
 		AsteroidShape s = new AsteroidShape();
+		List<Vector> vertices=generateEquilateralBase(3);
+		s.addVertice(new Vector(0,1,0));//Center
+		s.addVertices(vertices);//Base
+		//TODO add Faces
 		return s;
 		
+	}
+	private List<Vector> generateEquilateralBase(int nVertex){
+		return this.generateEquilateralBase(0,0,0,1,nVertex);
+	}
+	private List<Vector> generateEquilateralBase(float centerX,float centerY,float centerZ,float radius, int nVertex){
+		List<Vector> vertex=new ArrayList<Vector>(nVertex);
+		double x,z;
+		 for(int i=0; i<nVertex; i++) {
+		      x=centerX+radius*Math.cos(2*Math.PI*i/nVertex);
+		      z=centerZ+radius*Math.sin(2*Math.PI*i/nVertex);
+		      vertex.add(new Vector((float)x,centerY,(float)z));
+		    }
+		return vertex;
 	}
 }
