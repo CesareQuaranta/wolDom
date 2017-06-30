7bce60a92a547f97c452f0864f42e5dc65349c5e
cesare.quaranta <cesare.quaranta@pronetics.it>
2017-07-01 01:30:17 +0200

    init Metod

--- src/main/java/edu/wol/dom/WolContainer.java
+++ src/main/java/edu/wol/dom/WolContainer.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 
 import edu.wol.dom.space.Position;
 public interface  WolContainer<T extends WorldContainer<E,Position>,E extends WolEntity> extends Runnable {
-	
+	public void init() throws Exception;
 	public void insertEntity(E entity,long wolID,Position position) throws IOException, Exception;
 	public Prospective generateNewWol() throws IOException, Exception;
 	public Collection<Phenomen<E>> getAllPhenomen(long wolID)throws IOException, Exception;
