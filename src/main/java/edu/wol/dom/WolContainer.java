120c5b4dad6b64f16e33c93d639bd9786b9cfc92
cesare.quaranta <cesare.quaranta@pronetics.it>
2017-07-01 01:19:44 +0200

    WolContainer -> Interface

--- src/main/java/edu/wol/dom/WolContainer.java
+++ src/main/java/edu/wol/dom/WolContainer.java
@@ -2,12 +2,11 @@ package edu.wol.dom;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 
 import edu.wol.dom.space.Position;
-public abstract class WolContainer<T extends WorldContainer<E,Position>,E extends WolEntity> implements Runnable {
+public interface  WolContainer<T extends WorldContainer<E,Position>,E extends WolEntity> extends Runnable {
 	
-	public abstract void insertEntity(E entity,long wolID,Position position) throws IOException, Exception;
-	public abstract Prospective generateNewWol() throws IOException, Exception;
-	public abstract Collection<Phenomen<E>> getAllPhenomen(long wolID)throws IOException, Exception;
+	public void insertEntity(E entity,long wolID,Position position) throws IOException, Exception;
+	public Prospective generateNewWol() throws IOException, Exception;
+	public Collection<Phenomen<E>> getAllPhenomen(long wolID)throws IOException, Exception;
 }
