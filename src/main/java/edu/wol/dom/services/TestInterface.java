a22bfcb2fe89e9409b571f4f8ec3d1d77f9c11b4
cesare.quaranta <cesare.quaranta@pronetics.it>
2017-07-24 20:48:17 +0200

    Interfaccia generazione isola

--- src/main/java/edu/wol/dom/services/TestInterface.java
+++ src/main/java/edu/wol/dom/services/TestInterface.java
@@ -3,7 +3,9 @@ package edu.wol.dom.services;
 import java.io.IOException;
 
 import edu.wol.dom.shape.AsteroidShape;
+import edu.wol.dom.shape.PlaneShape;
 
 public interface TestInterface {
 	public AsteroidShape generateHidrogenGemShape(float length1,float length2,float length3, float length4,double angoloX,double angolo3X,double angolo1Y,double angolo2Y,double angolo3Y,double angoloZ,float correctXZ,float correctY) throws IOException, Exception;
+	public PlaneShape generateIsland(int lod,double roughness,double maxHeigth);
 }
