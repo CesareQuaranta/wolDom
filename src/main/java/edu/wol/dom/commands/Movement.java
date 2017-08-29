d812f68db55d776c6e5e74758f8dc8105109cbea
cesare.quaranta <cesare.quaranta@pronetics.it>
2017-08-29 13:15:57 +0200

    new Command

---
+++ src/main/java/edu/wol/dom/commands/Movement.java
@@ -0,0 +1,15 @@
+package edu.wol.dom.commands;
+
+import edu.wol.dom.space.Position;
+
+public class Movement implements Command{
+	private Position position;
+	
+	public Position getPosition() {
+		return position;
+	}
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+}
